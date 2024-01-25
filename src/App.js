284ae16d277e1ed908808bce2a59dd17c78adfa2
MikeYoulend <mikeyoulend@gmail.com>
2024-01-25 17:46:38 +0100

    Portfolio

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Intro from "./components/Navbar/Intro/intro";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
+			<Intro />
 		</div>
 	);
 }
---
+++ src/components/Navbar/Intro/intro.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./intro.css";
+import bg from "../../../assets/image.png";
+
+const Intro = () => {
+	return (
+		<section id="intro">
+			<div className="IntroContent"></div>
+			<img src="" className="bg" />
+		</section>
+	);
+};
+
+export default Intro;
