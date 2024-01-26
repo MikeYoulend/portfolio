041752bf6b86f5c60b1b13832636432a0ad3919a
MikeYoulend <mikeyoulend@gmail.com>
2024-01-26 17:05:05 +0100

    Portfolio

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -5,7 +5,13 @@ import bg from "../../assets/image.png";
 const Intro = () => {
 	return (
 		<section id="intro">
-			<div className="IntroContent"></div>
+			<div className="IntroContent">
+				<span className="hello">Hello,</span>
+				<span className="introText">
+					I'm <span className="introName">Smith</span> <br />
+					Website Designer
+				</span>
+			</div>
 			<img src={bg} alt="profile" className="bg" />
 		</section>
 	);
