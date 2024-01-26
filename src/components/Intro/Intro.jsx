f097a4872fbc8605f886fae106043aa2abde2d92
MikeYoulend <mikeyoulend@gmail.com>
2024-01-26 17:19:15 +0100

    Portfolio

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./intro.css";
 import bg from "../../assets/image.png";
+import btnImg from "../../assets/hireme.png";
+import { Link } from "react-scroll";
 
 const Intro = () => {
 	return (
@@ -11,6 +13,15 @@ const Intro = () => {
 					I'm <span className="introName">Smith</span> <br />
 					Website Designer
 				</span>
+				<p className="introPara">
+					I am a skilled web designer with experience in creating visually
+					appealing and user friendly websites.
+				</p>
+				<Link>
+					<button className="btn">
+						<img src={btnImg} alt="Hire me" /> Hire me
+					</button>
+				</Link>
 			</div>
 			<img src={bg} alt="profile" className="bg" />
 		</section>
