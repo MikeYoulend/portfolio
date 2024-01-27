df3f9d0c20aa6a94093d5f2b23bccfdf476293a0
MikeYoulend <mikeyoulend@gmail.com>
2024-01-27 18:23:08 +0100

    Works Section First Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Intro from "./components/Intro/Intro";
 import Skills from "./components/Skills/Skills";
+import Works from "./components/Works/Works";
 import Navbar from "./components/navbar/Navbar";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
 			<Navbar />
 			<Intro />
 			<Skills />
+			<Works />
 		</div>
 	);
 }
---
+++ src/components/Works/Works.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Works = () => {
+	return <section id="works">Works Done</section>;
+};
+
+export default Works;
