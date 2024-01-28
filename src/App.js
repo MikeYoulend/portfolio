7c53a08976b38a2abf42850a3c6a8719df962e12
MikeYoulend <mikeyoulend@gmail.com>
2024-01-28 16:11:18 +0100

    Portfolio

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 import Intro from "./components/Intro/Intro";
 import Skills from "./components/Skills/Skills";
 import Works from "./components/Works/Works";
@@ -12,6 +13,7 @@ function App() {
 			<Skills />
 			<Works />
 			<Contact />
+			<Footer />
 		</div>
 	);
 }
