2610665126ad75a102b0b11845c1155077175640
MikeYoulend <mikeyoulend@gmail.com>
2024-01-28 15:17:29 +0100

    Portfolio

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,6 +4,10 @@ import Walmart from "../../assets/walmart.png";
 import Adobe from "../../assets/adobe.png";
 import Microsoft from "../../assets/microsoft.png";
 import Facebook from "../../assets/facebook.png";
+import FacebookIcon from "../../assets/facebook-icon.png";
+import InstagramIcon from "../../assets/instagram.png";
+import LinkedinIcon from "../../assets/LinkedinIcon.png";
+import WhatsappIcon from "../../assets/WhatsappIcon.png";
 
 const Contact = () => {
 	return (
@@ -23,7 +27,34 @@ const Contact = () => {
 					<img src={Facebook} alt="Client" className="clientImg" />
 				</div>
 			</div>
-			<div id="contact"></div>
+			<div id="contact">
+				<h1 className="contactPageTitle">Contact Me</h1>
+				<span className="contactDesc">
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum quos
+					quia necessitatibus facere incidunt, aliquid harum facilis at delectus
+					earum consequuntur fugit minima optio ipsam illum quo velit dolore
+					hic!
+				</span>
+				<form action="" className="contactForm">
+					<input type="text" className="name" placeholder="Your Name" />
+					<input type="email" className="email" placeholder="Your Email" />
+					<textarea
+						className="msg"
+						name="message"
+						rows="5"
+						placeholder="Your Message"
+					></textarea>
+					<button type="submit" value="Send" className="submitBtn">
+						Submit
+					</button>
+					<div className="links">
+						<img src={FacebookIcon} alt="Facebook" className="link" />
+						<img src={InstagramIcon} alt="Instragram" className="link" />
+						<img src={LinkedinIcon} alt="Linkedin" className="link" />
+						<img src={WhatsappIcon} alt="Whatsapp" className="link" />
+					</div>
+				</form>
+			</div>
 		</section>
 	);
 };
